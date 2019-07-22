d950a6dfeaf75b6c72fe2ffbd65960e69f510142
Devilsen <375913436@qq.com>
2019-07-22 10:52:52 +0800

    加入一个yuv转rgb的方法

--- czxing/src/main/java/me/devilsen/czxing/util/SaveImageUtil.java
+++ czxing/src/main/java/me/devilsen/czxing/util/SaveImageUtil.java
@@ -1,10 +1,14 @@
 package me.devilsen.czxing.util;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.os.Environment;
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -32,6 +36,8 @@ public class SaveImageUtil {
         saveImage(bmp);
 
         bmp = getBinaryzationBitmap(bmp);
+//        Bitmap bmp = rawByteArray2RGBABitmap2(data, rowWidth, width);
+
         saveImage(bmp);
 
         bmp.recycle();
@@ -96,6 +102,42 @@ public class SaveImageUtil {
             }
         }
         return bitmap;
+    }
+
+    public static Bitmap rawByteArray2RGBABitmap(byte[] data, int width, int height) {
+        int frameSize = width * height;
+        int[] rgba = new int[frameSize];
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++) {
+                int y = (0xff & ((int) data[i * width + j]));
+                int u = (0xff & ((int) data[frameSize + (i >> 1) * width + (j & ~1) + 0]));
+                int v = (0xff & ((int) data[frameSize + (i >> 1) * width + (j & ~1) + 1]));
+                y = y < 16 ? 16 : y;
+                int r = Math.round(1.164f * (y - 16) + 1.596f * (v - 128));
+                int g = Math.round(1.164f * (y - 16) - 0.813f * (v - 128) - 0.391f * (u - 128));
+                int b = Math.round(1.164f * (y - 16) + 2.018f * (u - 128));
+                r = r < 0 ? 0 : (r > 255 ? 255 : r);
+                g = g < 0 ? 0 : (g > 255 ? 255 : g);
+                b = b < 0 ? 0 : (b > 255 ? 255 : b);
+                rgba[i * width + j] = 0xff000000 + (b << 16) + (g << 8) + r;
+            }
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bmp.setPixels(rgba, 0, width, 0, 0, width, height);
+        return bmp;
+    }
+
+    public static Bitmap rawByteArray2RGBABitmap2(byte[] data, int width, int height) {
+        Bitmap bmp = null;
+        try {
+            YuvImage image = new YuvImage(data, ImageFormat.NV21, width, height, null);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            image.compressToJpeg(new Rect(0, 0, width, height), 80, stream);
+            bmp = BitmapFactory.decodeByteArray(stream.toByteArray(), 0, stream.size());
+            stream.close();
+        } catch (Exception ex) {
+            Log.e("Sys", "Error:" + ex.getMessage());
+        }
+        return bmp;
     }
 
     private static long time;
